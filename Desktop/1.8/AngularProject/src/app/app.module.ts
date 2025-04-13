1e3165f3a3a982ba8a2405cee7cfac9513dcf1d0
Yaroslav Nazarov <yaroslavnazarov9@gmail.com>
2025-04-13 11:58:40 +0300

    new-1.8

--- Desktop/1.8/AngularProject/src/app/app.module.ts
+++ Desktop/1.8/AngularProject/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
-import * as moment from 'moment';
+
 
 @NgModule({
   imports: [
